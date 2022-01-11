7364c037dffce87b18dbe8c88ec99deec5e5b15e
rksrajneesh <60956450+rksrajneesh@users.noreply.github.com>
2022-01-11 16:49:24 +0530

    Time: 2 ms (5.37%), Space: 6 MB (42.36%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        if(n==0){
+            return 1;
+        }
+        int m=n;
+        int mask=0;
+        while(m!=0){
+            mask = (mask<<1) | 1;
+            m = m>>1;
+        }
+        int ans = mask&(~n);
+        return ans;
+    }
+};
