d23eb9b7e702d029874c829ab2c82cf0271e8dba
rksrajneesh <60956450+rksrajneesh@users.noreply.github.com>
2022-06-19 15:18:50 +0530

    Added solution - LeetHub

---
+++ 4. Reverse Array Using Stack - GFG/4.-reverse-array-using-stack.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+class Solution{
+  public:
+    void reverseArray(int n, int* arr){
+        
+      //Your code here
+      stack<int> s;
+      for(int i=0;i<n;i++){
+          s.push(arr[i]);
+      }
+      int i=0;
+      while(s.empty()==false){
+          arr[i]=s.top();
+          i++;
+          s.pop();
+      }
+      //Reverse the array
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    Solution obj;
+    obj.reverseArray(n, arr);
+    
+    for(int i = 0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    
+    cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
