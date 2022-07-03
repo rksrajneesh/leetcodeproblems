8fb28c8857473c05811ce40337fa74a6551f911d
rksrajneesh <60956450+rksrajneesh@users.noreply.github.com>
2022-07-03 17:51:17 +0530

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        vector<int> v1;
+      vector<int> v2;
+      for(int i=0;i<n;i++){
+          if(arr[i]<0){
+              v1.push_back(arr[i]);
+          }else{
+              v2.push_back(arr[i]);
+          }
+      }
+      int j = v2.size();
+      for(int i=0;i<v2.size();i++){
+          arr[i] = v2[i];
+      }for(int i=0;i<v1.size();i++){
+          arr[i+j] = v1[i];
+          
+      }
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+  // } Driver Code Ends
